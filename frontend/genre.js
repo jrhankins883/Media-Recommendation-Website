ec1dbce27d57dd62fb7738ec259b1c184e1aa432
John Hankins <jrhankins883@yahoo.com>
2025-02-25 18:41:55 -0500

    I've added a background image and plan on making the menu more interactive. Not there yet

--- frontend/genre.js
+++ frontend/genre.js
@@ -1,2 +1,20 @@
 // URL of the Flask backend
-const BASE_URL = 'http://127.0.0.1:5000';
+
+then((movies) => {
+    movieList.innerHTML = ""; // clear previous movies
+    movies.forEach((movie) => {
+        const movieItem = document.createElement("div");
+        movieItem.classList.add("movie");
+
+        const movieImage = document.createElement("img");
+        movieImage.src = 'https://image.tmdb.org/t/p/w200${movie.poster_path}';
+        movieImage.alt = movie.title;
+
+        const movieTitle = document.createElement("p");
+        movieTitle.textContent = movie.title;
+
+        movieItem.appendChild(movieImage);
+        movieItem.appendChild(movieTitle);
+        movieItem.appendChild(movieItem);
+    })
+});
